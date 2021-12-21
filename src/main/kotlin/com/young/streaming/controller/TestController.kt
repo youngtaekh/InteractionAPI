cbc4cb6c761a4fe4ed4dd26b8ef59e8e1c6ca7c4
youngtaek <youngtaek@peoplev.co.kr>
2021-12-21 15:16:54 +0900

    Write TestController

--- src/main/kotlin/com/young/streaming/controller/TestController.kt
+++ src/main/kotlin/com/young/streaming/controller/TestController.kt
@@ -1,4 +1,24 @@
 package com.young.streaming.controller
 
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping(value = ["/test"])
 class TestController {
+
+    @GetMapping
+    fun test(): ResponseEntity<String> {
+        println("GET test")
+        return ResponseEntity.ok("OK")
+    }
+
+    @PutMapping("/put")
+    fun put(): ResponseEntity<String> {
+        println("PUT")
+        return ResponseEntity.ok("PUT OK")
+    }
 }
